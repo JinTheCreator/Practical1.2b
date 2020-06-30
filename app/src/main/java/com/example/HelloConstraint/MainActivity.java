3c9f4c9cc248cef20d6305905e68025a567c52ab
YeongYauJin <yeongyj-wm19@student.tarc.edu.my>
2020-06-30 18:57:29 +0800

    First by YeongYauJin

--- app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.practical_2_part_a;
+package com.example.HelloConstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,10 @@ private TextView mShowCount;
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
+
+
+
+
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_massage,Toast.LENGTH_SHORT);
 
@@ -28,6 +35,23 @@ private TextView mShowCount;
         ++mCount;
        if (mShowCount != null)
            mShowCount.setText(Integer.toString(mCount));
+    if(mCount>0)
+    {
+        findViewById(R.id.button_zero).setBackgroundColor(Color.RED);
+    }
+        if(mCount % 2 == 0)
+            view.setBackgroundColor(Color.BLUE);
+        else
+            view.setBackgroundColor(Color.GREEN);
+
+    }
+    public void zero(View view){
+        if(mCount !=0)
+        {
+            view.setBackgroundColor(Color.GRAY);
+        }
+        mCount = 0;
+        mShowCount.setText(Integer.toString(0));
 
     }
 }
